7fb9e08e3beb54caf9614dc71ab7740f797058cc
luis-rios <luis.rios.9815@gmail.com>
2022-03-26 20:29:59 -0600

    Mi primer commit: agregando funcionalidad básica y estilos como indica la rubrica.

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './App.css';
+import QuoteWindow from './components/QuoteWindow/QuoteWindow';
+import Quotes from './Quotes.json'
+
+
+
+function App() {
+  //*Código de javascript
+  // console.log(Quotes.quotes[0])
+  const generateRandomNumber = () => {
+    const randNum = Math.ceil(Math.random() * 101);
+    console.log(randNum)
+    return randNum
+  }
+
+  //*Uso de un estado para cambiar el indice del quote
+
+  const [indexQuote, changeIndexQuote] = useState(generateRandomNumber());
+
+  //!----------------------------------------------------
+  const handleIndex = () => {
+    changeIndexQuote(() => generateRandomNumber());
+  }
+
+
+  return (
+    <div className="App">
+      <QuoteWindow quote={Quotes.quotes[indexQuote].quote} author={Quotes.quotes[indexQuote].author} handle={handleIndex}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuoteWindow/QuoteWindow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './QuoteWindow.css'
+
+export default function QuoteWindow({quote, author, handle}) {
+  return (
+    <div className='QuoteWindow'>
+        <p className='QuoteText'>
+            {quote}
+        </p>
+        <h4 className='QuoteAuthor'>-{author}</h4>
+        <button className='QuoteButton' onClick={handle}>Click to generate</button>
+    </div>
+  )
+}
