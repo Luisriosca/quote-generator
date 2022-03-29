0469d04dc42b341a318273f51dd484ef14ad6140
luis-rios <luis.rios.9815@gmail.com>
2022-03-28 19:13:14 -0600

    fix an error

--- src/components/QuoteBoxHeader/QuoteBoxHeader.js
+++ src/components/QuoteBoxHeader/QuoteBoxHeader.js
@@ -5,9 +5,9 @@ export default function QuoteBoxHeader() {
   return (
     <div className='header'> 
         <div className='icons'>
-        <i class='bx bxs-circle bxs-circle1'></i>
-        <i class='bx bxs-circle bxs-circle2'></i>
-        <i class='bx bxs-circle bxs-circle3'></i>
+        <i className='bx bxs-circle bxs-circle1'></i>
+        <i className='bx bxs-circle bxs-circle2'></i>
+        <i className='bx bxs-circle bxs-circle3'></i>
         </div>
         <div>
             created by: <a target="_blank" href="https://luisrioscode.netlify.app/">luisrioscode</a>
